3547b97415ae3f1c6599719a2823945356c01c0e
Ganlv <ganlvtech@qq.com>
2018-10-26 13:17:41 +0800

    Rename github_pages_crawl.php

--- build_github_pages.sh
+++ build_github_pages.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 export BUILD_GITHUB_PAGES=true
-
 npm install
 npm run build
 
 sed -i "s/new Down52PojieCn({/& jsonUrl: '\/down_52pojie_cn\/list.json' /g" dist/index.html
 
-php php/github_pages_crawl.php
+pushd php/
+composer install
+php crawl.php
+popd
