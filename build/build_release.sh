f5fa60b304f4c0af44b29c57f844ff69cc115309
Ganlv <ganlvtech@qq.com>
2018-10-27 22:42:16 +0800

    dist add docs folder

--- build/build_release.sh
+++ build/build_release.sh
@@ -11,6 +11,7 @@ npm run build
 sed -i "s/new Down52PojieCn({/& routerMode: 'history', requestType: 'jsonp' /g" dist/index.html
 sed -i "s/new Down52PojieCn({/& routerMode: 'history', requestType: 'jsonp' /g" dist/.fancyindex/footer.html
 
+cp docs dist/ -R
 cp php dist/ -R
 rm dist/php/crawl.php
 cp LICENSE dist/
